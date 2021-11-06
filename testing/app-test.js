4f093a481d1f56d944af11a3b29dce74b861ba27
Ivan <ivansacul@gmail.com>
2021-11-06 09:23:52 -0400

    add read.js method

--- testing/app-test.js
+++ testing/app-test.js
@@ -4,6 +4,7 @@ const Task = require('../api/task.js');
 const add = require('../api/add.js');
 const edit = require('../api/edit.js');
 const del = require('../api/del.js');
+const read = require('../api/read.js');
 
 const memoryTasks = require('../data/memoryTasks.js');
 const memoryProjects = require('../data/memoryProjects.js');
@@ -20,22 +21,23 @@ const memoryProjects = require('../data/memoryProjects.js');
    
   task = new Task('task4', 'oleg4', '3-12-1979');
   console.log(await add('task', task));
- 
+  
   //task = new Task('task4', 'oleg4', '3-12-1979');
   console.log(await add('task', new Task('task5', 'oleg5', '3-12-1979')));
- 
-  console.dir(memoryTasks);
+  
+//  console.dir(memoryTasks);
   
   console.log(await del('task', 2));
-  console.dir(memoryTasks);
+  //console.dir(memoryTasks);
   
   task = new Task('task6', 'oleg6', '3-12-1979');
   console.log(await add('task', task));
-  console.dir(memoryTasks);
+  //console.dir(memoryTasks);
   
   task = new Task('task11', 'oleg11', '3-12-1979');
   console.log(await edit('task', 1, task));
-  console.dir(memoryTasks);
   
-  console.dir(JSON.stringify(memoryTasks));
+  
+  console.dir(await read('task', 'all'));
+  console.dir(await read('task', 3));
 })();
