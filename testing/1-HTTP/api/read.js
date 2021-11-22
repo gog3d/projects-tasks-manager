23a09a42915b195180b792ef95eba8fdb962ef68
gog3d <gog3d@mail.ru>
2021-11-22 10:33:59 -0500

    gitignore testing

--- testing/1-HTTP/api/read.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const memory = require('../memory.js');
-
-module.exports = async (name) => {
-  const shape = memory.get(name);
-  if (!shape) return 'Shape is not found';
-  return shape;
-};
--- testing/crypto.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const crypto = require('crypto');
-const id1 = crypto.randomBytes(16).toString('hex');
-const id2 = crypto.randomUUID();
-console.log({ id1, id2 });
